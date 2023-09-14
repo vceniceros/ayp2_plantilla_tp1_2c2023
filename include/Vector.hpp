3f843c13283ea4f2d9c4e3dd854fcf116ec1fdfe
RDJulian <jruizd@fi.uba.ar>
2023-09-14 01:26:14 -0300

    Fixes

--- include/Vector.hpp
+++ include/Vector.hpp
@@ -17,7 +17,7 @@ class VectorException : public std::exception {
 
 class Vector {
 private:
-    Item* datos;
+    Item** datos;
     size_t cantidadDatos;
     size_t tamanioMaximo;
 public:
@@ -26,21 +26,21 @@ public:
 
     // Pre: -
     // Post: Agrega el dato al final del vector.
-    void alta(Item dato);
+    void alta(Item* dato);
 
     // Pre: El indice debe ser menor o igual que la cantidad de datos.
     // Post: Agrega el dato antes del dato en la posicion indicada, moviendo todos los elementos siguientes. Si el
     // indice es igual a la cantidad de datos, simplemente agrega al final.
-    void alta(Item dato, size_t indice);
+    void alta(Item* dato, size_t indice);
 
     // Pre: El vector no puede estar vacio.
     // Post: Elimina el ultimo dato.
-    Item baja();
+    Item* baja();
 
     // Pre: El vector no puede estar vacio. El indice debe ser menor que la cantidad de datos.
     // Post: Elimina el dato en la posicion indicada, moviendo todos los elementos siguientes. Si el indice es igual a
     // la cantidad de datos - 1, simplemente elimina el ultimo dato.
-    Item baja(size_t indice);
+    Item* baja(size_t indice);
 
     // Pre: -
     // Post: Devuelve true si el vector esta vacio (es decir, si no hay datos).
@@ -52,7 +52,7 @@ public:
 
     // Pre: El vector no puede estar vacio. El indice debe ser menor que la cantidad de datos.
     // Post: Devuelve una referencia al dato deseado.
-    Item& operator[](size_t indice);
+    Item*& operator[](size_t indice);
 
     // Destructor.
     ~Vector();
--- tests/VectorTests.cpp
+++ tests/VectorTests.cpp
@@ -4,100 +4,137 @@
 class VectorTests : public ::testing::Test {
 protected:
     Vector vector{};
-    Item dato{};
+    Item* dato{};
+    Item* dato1 = new Item("1", "");
+    Item* dato2 = new Item("2", "");
+    Item* dato3 = new Item("3", "");
+
+    void liberar();
 };
 
-TEST_F(VectorTests, Alta) {
-    vector.alta(Item("1", ""));
-    EXPECT_TRUE(vector[0] == "1");
+void VectorTests::liberar() {
+    delete dato1;
+    delete dato2;
+    delete dato3;
+}
 
-    vector.alta(Item("2", ""));
-    EXPECT_TRUE(vector[1] == "2");
+TEST_F(VectorTests, Alta) {
+    vector.alta(dato1);
+    EXPECT_TRUE(*vector[0] == "1");
+
+    vector.alta(dato2);
+    EXPECT_TRUE(*vector[1] == "2");
+
+    liberar();
 }
 
 TEST_F(VectorTests, AltaIndice) {
-    vector.alta(Item("1", ""), 0);
-    EXPECT_TRUE(vector[0] == "1");
+    vector.alta(dato1, 0);
+    EXPECT_TRUE(*vector[0] == "1");
 
-    vector.alta(Item("2", ""), 0);
-    EXPECT_TRUE(vector[0] == "2");
+    vector.alta(dato2, 0);
+    EXPECT_TRUE(*vector[0] == "2");
+
+    liberar();
 }
 
 TEST_F(VectorTests, AltaIndiceManejaIndiceMayorACantidadDeDatos) {
-    EXPECT_THROW(vector.alta(Item("1", ""), 1), VectorException);
+    EXPECT_THROW(vector.alta(dato1, 1), VectorException);
+
+    liberar();
 }
 
 
 TEST_F(VectorTests, Baja) {
-    vector.alta(Item("1", ""));
+    vector.alta(dato1);
 
     dato = vector.baja();
-    EXPECT_TRUE(dato == "1");
+    EXPECT_TRUE(*dato == "1");
+
+    liberar();
 }
 
 TEST_F(VectorTests, BajaManejaVectorVacio) {
     EXPECT_THROW(vector.baja(), VectorException);
+
+    liberar();
 }
 
 TEST_F(VectorTests, BajaIndice) {
-    vector.alta(Item("1", ""));
-    vector.alta(Item("2", ""));
+    vector.alta(dato1);
+    vector.alta(dato2);
 
     dato = vector.baja(0);
-    EXPECT_TRUE(dato == "1");
+    EXPECT_TRUE(*dato == "1");
 
     dato = vector.baja(0);
-    EXPECT_TRUE(dato == "2");
+    EXPECT_TRUE(*dato == "2");
+
+    liberar();
 }
 
 TEST_F(VectorTests, BajaIndiceManejaVectorVacio) {
     EXPECT_THROW(vector.baja(0), VectorException);
+
+    liberar();
 }
 
 TEST_F(VectorTests, BajaIndiceManejaIndiceMayorOIgualACantidadDeDatos) {
-    vector.alta(Item("1", ""));
+    vector.alta(dato1);
 
     EXPECT_THROW(vector.baja(1), VectorException);
+
+    liberar();
 }
 
 TEST_F(VectorTests, Vacio) {
     EXPECT_TRUE(vector.vacio());
 
-    vector.alta(Item("1", ""));
+    vector.alta(dato1);
     EXPECT_FALSE(vector.vacio());
 
     vector.baja();
     EXPECT_TRUE(vector.vacio());
+
+    liberar();
 }
 
 TEST_F(VectorTests, Tamanio) {
     EXPECT_EQ(vector.tamanio(), 0);
 
-    vector.alta(Item("1", ""));
-    vector.alta(Item("2", ""));
-    vector.alta(Item("3", ""));
+    vector.alta(dato1);
+    vector.alta(dato2);
+    vector.alta(dato3);
     EXPECT_EQ(vector.tamanio(), 3);
 
     vector.baja();
     EXPECT_EQ(vector.tamanio(), 2);
+
+    liberar();
 }
 
 TEST_F(VectorTests, OperadorAcceso) {
-    vector.alta(Item("1", ""));
-    vector.alta(Item("2", ""), 0);
-    vector.alta(Item("3", ""));
+    vector.alta(dato1);
+    vector.alta(dato2, 0);
+    vector.alta(dato3);
     dato = vector.baja(0);
 
-    EXPECT_TRUE(vector[1] == "3");
-    EXPECT_TRUE(dato == "2");
+    EXPECT_TRUE(*vector[1] == "3");
+    EXPECT_TRUE(*dato == "2");
+
+    liberar();
 }
 
 TEST_F(VectorTests, OperadorAccesoManejaVectorVacio) {
     EXPECT_THROW(vector[0], VectorException);
+
+    liberar();
 }
 
 TEST_F(VectorTests, OperadorAccesoManejaIndiceMayorOIgualACantidadDeDatos) {
-    vector.alta(Item("1", ""));
+    vector.alta(dato1);
 
     EXPECT_THROW(vector[1], VectorException);
+
+    liberar();
 }
